d38ab7d725efc68268b052f55f06d798d7ef53b0
Mewneme <lili75088@gmail.com>
2020-04-06 15:26:01 +0800

    1

---
+++ a20200406-06-session.php
@@ -0,0 +1,11 @@
+<?php
+//是否啟動過 session 功能，若沒有啟動
+if(! isset($_SESSION)){
+    session_start(); // 啟動 session 功能
+}
+if(isset($_SESSION['myvar'])){
+    $_SESSION['myvar']++;
+}else{
+    $_SESSION['myvar'] = 1;
+}
+echo $_SESSION['myvar'];
